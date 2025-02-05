036cdf3a6908fe62470d55a2323024cd7451650f
Suryakant Mudke <suryakantmudke1234@gmail.com>
2025-02-06 01:03:00 +0530

    2: 1st commit on feature 1

--- src/Hello_world.java
+++ src/Hello_world.java
@@ -1,7 +1,11 @@
+import java.sql.SQLOutput;
+
 public class  Hello_world {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("Feature 1 added");
     }
 
 }
